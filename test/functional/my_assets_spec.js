0d44fa528236fee0b8d0a512ce6e5f22069d143b
Alan Gardner <alan@armoin.com>
2014-06-23 10:57:43 +0100

    Adding more functional tests to the My Assets page
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- test/functional/my_assets_spec.js
+++ test/functional/my_assets_spec.js
@@ -11,16 +11,39 @@ describe('My Assets page', function () {
     this.browser = new Browser({site: 'http://localhost:3001'});
   });
 
-  before(function (done) {
-    this.browser.visit('/', done);
-  });
+  describe('GET list', function () {
+    before(function (done) {
+      this.browser.visit('/', done);
+    });
 
-  it("is successful", function () {
-    expect(true).to.be.true;
-  });
+    it("is successful", function () {
+      expect(this.browser.statusCode).to.eql(200);
+    });
 
-  it("displays all the user's assets", function () {
-    expect(this.browser.queryAll('.asset').length).to.eql(5);
+    it("displays all the user's assets", function () {
+      expect(this.browser.queryAll('.asset').length).to.eql(5);
+    });
+
+    describe('an asset', function () {
+      beforeEach(function () {
+        this.asset = this.browser.query('.asset');
+      });
+
+      it("has a title", function () {
+        var title = this.asset.querySelector('.title').innerHTML;
+        expect(title).to.equal('Cathedral');
+      });
+
+      it("has a image representing the asset", function () {
+        var url = this.asset.querySelector('img').attributes.src.value;
+        expect(url).to.equal('/images/st_giles.jpg');
+      });
+
+      it("has a link to more details", function () {
+        var viewDetails = this.asset.querySelector('a.details');
+        expect(viewDetails).not.to.be.null;
+      });
+    });
   });
 
   after(function (done) {
